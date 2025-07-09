a0f11e6926b3a53a5fa220180da76d05fe7acadd
miaolei20 <ml20061023@outlook.com>
2025-07-09 22:39:55 +0800

    improve struction

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -18,35 +18,38 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
-  spec = {
-    { import = "plugins.ui" },
-    { import = "plugins.files" },
-    { import = "plugins.editor" },
-    { import = "plugins.tools" },
-    { import = "plugins.coding" },
-    { import = "plugins.treesitter"},
-    { import = "plugins.git"},
-    { import = "plugins.markdown"},
-  },
+	spec = {
+		{ import = "plugins.ui" },
+		{ import = "plugins.editor" },
+		{ import = "plugins.tools" },
+		{ import = "plugins.coding" },
+		{ import = "plugins.treesitter" },
+		{ import = "plugins.git" },
+		{ import = "plugins.markdown" },
+	},
 
-  install = { colorscheme = { "tokyonight" } },
+	checker = {
+		enabled = true,
+		notify = false,
+		frequency = 86400,
+	},
 
-  checker = {
-    enabled = true,
-    notify = false,
-    frequency = 86400,
-  },
+	performance = {
+		cache = { enabled = true },
+		reset_packpath = false,
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 
-  performance = {
-    cache = { enabled = true },
-    reset_packpath = false,
-    rtp = {
-      disabled_plugins = {
-        "gzip", "matchit", "matchparen", "netrwPlugin",
-        "tarPlugin", "tohtml", "tutor", "zipPlugin",
-      },
-    },
-  },
-
-  change_detection = { enabled = true, notify = false },
+	change_detection = { enabled = true, notify = false },
 })
