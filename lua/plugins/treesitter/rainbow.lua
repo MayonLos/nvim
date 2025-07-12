0c265395860f738bcd917354111d3b03af009c57
miaolei20 <ml20061023@outlook.com>
2025-07-13 01:38:39 +0800

    improve rainbow

--- lua/plugins/treesitter/rainbow.lua
+++ lua/plugins/treesitter/rainbow.lua
@@ -1,28 +1,20 @@
 return {
-	{
-		"HiPhish/rainbow-delimiters.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter" }, -- Required for Tree-sitter support
-		event = { "BufReadPre", "BufNewFile" }, -- Lazy loading on buffer events
-		config = function()
-			-- Defer setup to reduce startup impact
-			vim.schedule(function()
-				local rainbow_delimiters = require("rainbow-delimiters.setup")
-				rainbow_delimiters.setup({
-					highlight = {
-						"RainbowDelimiterRed",
-						"RainbowDelimiterYellow",
-						"RainbowDelimiterBlue",
-						"RainbowDelimiterOrange",
-						"RainbowDelimiterGreen",
-						"RainbowDelimiterViolet",
-						"RainbowDelimiterCyan",
-					},
-				})
-			end)
-		end,
-		-- Conditionally load only when Tree-sitter is available
-		cond = function()
-			return pcall(require, "nvim-treesitter")
-		end,
+	"HiPhish/rainbow-delimiters.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
 	},
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		require("rainbow-delimiters.setup").setup({
+			highlight = {
+				"RainbowDelimiterRed",
+				"RainbowDelimiterYellow",
+				"RainbowDelimiterBlue",
+				"RainbowDelimiterOrange",
+				"RainbowDelimiterGreen",
+				"RainbowDelimiterViolet",
+				"RainbowDelimiterCyan",
+			},
+		})
+	end,
 }
