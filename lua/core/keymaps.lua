a6ab2e0956821dd1641018ef8ac3618895dbe7a2
miaolei20 <ml20061023@outlook.com>
2025-07-17 22:52:14 +0800

    remove some unused plugings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,32 +7,26 @@ function M.setup()
 	-- ▍通用
 	map("n", "<leader>nh", ":nohlsearch<CR>", { desc = "Clear search highlight" })
 
-	-- ▍Buffer 操作（barbar.nvim）
-	map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", { desc = "Previous buffer" })
-	map("n", "<A-.>", "<Cmd>BufferNext<CR>", { desc = "Next buffer" })
+	-- ▍Buffer 操作
+	map("n", "<leader>bn", "<Cmd>BufferNext<CR>", { desc = "Next buffer" })
+	map("n", "<leader>bp", "<Cmd>BufferPrevious<CR>", { desc = "Previous buffer" })
+	map("n", "<leader>bl", "<Cmd>BufferLast<CR>", { desc = "Go to last buffer" })
+	map("n", "<leader>bb", "<Cmd>BufferPick<CR>", { desc = "Pick buffer" })
+	map("n", "<leader>bd", "<Cmd>BufferClose<CR>", { desc = "Close buffer" })
+	map("n", "<leader>bx", "<Cmd>BufferPickDelete<CR>", { desc = "Pick & delete buffer" })
+	map("n", "<leader>bm", "<Cmd>BufferPin<CR>", { desc = "Pin/unpin buffer" })
+	map("n", "<leader>bb", "<Cmd>BufferPick<CR>", { desc = "Pick buffer" })
 
-	map("n", "<A-c>", "<Cmd>BufferClose<CR>", { desc = "Close buffer" })
-	map("n", "<A-p>", "<Cmd>BufferPin<CR>", { desc = "Pin/unpin buffer" })
-
-	map("n", "<C-p>", "<Cmd>BufferPick<CR>", { desc = "Pick buffer" })
-	map("n", "<C-s-p>", "<Cmd>BufferPickDelete<CR>", { desc = "Pick & delete buffer" })
-
-	for i = 1, 9 do
-		map("n", "<A-" .. i .. ">", "<Cmd>BufferGoto " .. i .. "<CR>", { desc = "Go to buffer " .. i })
-	end
-	map("n", "<A-0>", "<Cmd>BufferLast<CR>", { desc = "Go to last buffer" })
-
-	map("n", "<leader>bo", "<Cmd>BufferOrderByBufferNumber<CR>", { desc = "Sort: buffer number" })
-	map("n", "<leader>bn", "<Cmd>BufferOrderByName<CR>", { desc = "Sort: name" })
-	map("n", "<leader>bd", "<Cmd>BufferOrderByDirectory<CR>", { desc = "Sort: directory" })
-	map("n", "<leader>bl", "<Cmd>BufferOrderByLanguage<CR>", { desc = "Sort: language" })
-	map("n", "<leader>bw", "<Cmd>BufferOrderByWindowNumber<CR>", { desc = "Sort: window number" })
-
-	--  更多模块（占位）
-	-- LSP
-	-- map("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Go to definition" })
-	-- Git
-	-- map("n", "<leader>gs", "<Cmd>Neogit<CR>", { desc = "Git: status" })
+	-- ▍Buffer 排序
+	map("n", "<leader>bsn", "<Cmd>BufferOrderByName<CR>", { desc = "Sort buffers by name" })
+	map("n", "<leader>bsd", "<Cmd>BufferOrderByDirectory<CR>", { desc = "Sort buffers by directory" })
+	map("n", "<leader>bsl", "<Cmd>BufferOrderByLanguage<CR>", { desc = "Sort buffers by language" })
+	map(
+		"n",
+		"<leader>bsw",
+		"<Cmd>BufferOrderByWindowNumber<CR>",
+		{ desc = "Sort buffers by window number" }
+	)
 end
 
 return M
